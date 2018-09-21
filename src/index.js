63c00d01d5eae01da2d5d26851c2313f29fc6640
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-09-21 04:42:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-import React , { Component as C } from 'react';
+import React from 'react';
 import { render as r } from 'react-dom';
 
-class Stater2 extends C {
- render() {
-   return  <h2>{ this.props.v }</h2>;
- }
-}
-class Stater  extends C  {
- constructor() {
-   super();
-   this.state = {v: 'Hello'};
- }
- click() {
-   this.setState({v: 'Hi, man!'});
- }
- render() {
-   return (<div>
-     <h1 onClick={this.click.bind(this)}>{ this.state.v }</h1>
-     <Stater2 v={ this.state.v }/>
-   </div>);
- }
-}
-
-r(
-  <Stater  />,
-  document.querySelector('.cont')
-); // https://kodaktor.ru/min_react_webpack4new
+const exps = [10, 1, 2, 3, 4, 5];
+const Deg = ({ base: b, exp: e }) =>
+  <h1><span>{ b }</span><sup>{ e }</sup> = { b ** e }</h1>;
+const Degs = ({ a }) =>
+  <ul>{ a.map((v, i) => <li><Deg base="2" exp={ v } key={ i } /></li>) }</ul>;
+r(<Degs a={ exps } />,
+document.querySelector('.cont'));

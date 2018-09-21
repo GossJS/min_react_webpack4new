4beed5556a529454208502409a9a3287bf46f31e
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-09-21 04:51:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component as C } from 'react';
 import { render as r } from 'react-dom';
 
-const exps = [10, 1, 2, 3, 4, 5];
 const Deg = ({ base: b, exp: e }) =>
   <h1><span>{ b }</span><sup>{ e }</sup> = { b ** e }</h1>;
 const Degs = ({ a }) =>
   <ul>{ a.map((v, i) => <li><Deg base="2" exp={ v } key={ i } /></li>) }</ul>;
-r(<Degs a={ exps } />,
+
+class App extends C {
+  constructor() {
+    super();
+    this.state = {
+      exps: [10, 1, 2, 3, 4, 5]
+    }
+  }
+  click(){
+    this.setState( ({ exps }) => ({ exps: [ ...exps, exps[exps.length-1]+1] }));
+  }
+  render (){
+    return (<div>
+      <button onClick={this.click.bind(this)}>⬆︎</button>
+      <Degs a={ this.state.exps } />
+    </div>);
+  }
+}
+
+r(<App />,
 document.querySelector('.cont'));
